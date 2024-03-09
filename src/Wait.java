4997f96196d4a9f6af6aab24bee5589502387229
sword4869 <1084662708@qq.com>
2024-03-09 23:48:19 +0800

    1

---
+++ src/Wait.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Wait {
+    static Object lock = new Object();
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            synchronized(lock){
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("t1 is notified");
+            }
+        }, "t1");
+        t1.start();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            synchronized(lock){
+                lock.notify();  
+                // notify()方法不会释放锁，所以t1线程不会被唤醒
+                // 后面如果有其它耗时操作，t1线程被唤醒后也不会立即执行
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }, "t2").start();
+    }
+}
