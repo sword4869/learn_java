2e4a110376c159dad5011401d1a0544ef6f34bf8
sword4869 <1084662708@qq.com>
2024-03-14 10:35:58 +0800

    1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,18 +1,4 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
-import java.util.Stack;
-
 public class Test {
     public static void main(String[] args) {
-        List<Character> stack = new Stack<>();
-        stack('c');
     }
 }
