d1bc9ac51b71f7ecb748d6cddb357b06d68ef9ea
sword4869 <1084662708@qq.com>
2024-06-01 11:04:50 +0800

    0601-am11

---
+++ docs/idea操作/找pom.md
@@ -0,0 +1,9 @@
+![alt text](../../images/image-207.png)
+
+![alt text](../../images/image-422.png)
+
+![alt text](../../images/image-423.png)
+
+![alt text](../../images/image-424.png)
+
+![alt text](../../images/image-425.png)
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,19 +3,25 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Test {
-    public static void main(String[] args){
-        List<File> files = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
-            File file = new File("file" + i + ".txt");
-            files.add(file);
+    public static void main(String[] args) throws IOException{
+        List<String> lines = Files.readAllLines(Path.of("D:\\git\\learn_java\\src\\main\\java\\Test.java"));
+        for(String line : lines){
+            System.out.println(line);
         }
+
+        // writeLines
+        List<String> lines2 = new ArrayList<>();
+        lines2.add("Hello");
+        lines2.add("World");
         
-        List<File> files2 = Stream.iterate(0, i -> i+1).limit(10).map(i -> new File("file" + i + ".txt")).collect(Collectors.toList());
+        // 导入commons-io包dependency: org.apache.commons:commons-io:1.3.2
     }
 }
