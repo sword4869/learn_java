6cba3cb206463eb83d30734235b63502e7af73e4
sword4869 <1084662708@qq.com>
2024-06-06 11:07:39 +0800

    0606-am11

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -12,16 +12,6 @@ import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) throws IOException{
-        List<String> lines = Files.readAllLines(Path.of("D:\\git\\learn_java\\src\\main\\java\\Test.java"));
-        for(String line : lines){
-            System.out.println(line);
-        }
 
-        // writeLines
-        List<String> lines2 = new ArrayList<>();
-        lines2.add("Hello");
-        lines2.add("World");
-        
-        // 导入commons-io包dependency: org.apache.commons:commons-io:1.3.2
     }
 }
